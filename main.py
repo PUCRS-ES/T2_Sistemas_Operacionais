1b903ebb953e0d3caf97672a18d9b75a5cd05e06
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2017-10-19 18:51:48 -0200

    Implementacao parcial da leitura do arquivo de entrada

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ class Process():
     quantidade_memoria = 0
     paginas = []
 
-    def __init__(self, _id, quantidade_memoria, paginas):
+    def __init__(self, _id, quantidade_memoria):
         self.id = _id
         self.quantidade_memoria = quantidade_memoria
 
 
 class ProcessManager():
-    processos = []
+    processos = { }
 
-    def carrega_processos():
+    def carrega_processos(self):
         with open("origem.txt", "r") as arquivo:
             linhas = arquivo.readlines()
-            for linha in linhas:
+            for index, linha in enumerate(linhas):
+                # Ignora 5 primeiras linhas que não são instrucoes de processos
+                if index < 5:
+                    continue
+                
                 instrucoes = linha.split(' ')
                 acao = instrucoes[0]
                 id_processo = instrucoes[1]
-                posicao_relativa_memoria[2]
+                posicao_relativa_memoria = int(instrucoes[2])
 
                 if acao == "C":
                     print("Criar processo")
+                    if id_processo not in self.processos:
+                        self.processos[id_processo] = Process(id_processo, posicao_relativa_memoria)
                 elif acao == "A":
                     print("Acesso/leitura")
                 elif acao == "M":
